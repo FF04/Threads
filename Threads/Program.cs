6904a2a024c8d5a13f29845e66e704f6d22e2a3f
Fabian <fabian.fried04@gmail.com>
2021-04-17 17:33:01 +0200

    Add project files.

---
+++ Threads/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System;
+using System.IO;
+using System.Threading;
+
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // erzeugung eines Threads
+            Thread t = new Thread(Print1); // zuweisung der Methode Print1()  (keine Klammern da ja kein ergebnis rein soll)
+            t.Start(); // Einmaliger start
+
+            // printet nur 0
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+
+
+            // Das Ergebnis: es werden 0er und 1er abwechselnd ausegeben!
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+        // Printet nur 1
+        static void Print1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+
+
+        }
+
+
+    }
+}
